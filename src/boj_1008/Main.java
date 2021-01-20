bb1a5b308d79f537c19cecd6e701750cef99c7f7
박종한 <jonghan1983@naver.com>
2021-01-20 16:01:34 +0900

    boj 1008 create

--- src/boj_1008/Main.java
+++ src/boj_1008/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int num1=sc.nextInt(),num2=sc.nextInt();
-		System.out.println(num1/num2);
+		System.out.println((double)num1/num2);
 	}
 
 }
