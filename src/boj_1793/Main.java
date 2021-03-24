3e088ba833bc66f2e409dbddd40f341cd2ce71f0
박종한 <jonghan1983@gmail.com>
2021-03-24 10:10:36 +0900

    BOJ 1793 DP 타일링

---
+++ src/boj_1793/Main.java
@@ -0,0 +1,28 @@
+package boj_1793;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+		BigInteger DP[]=new BigInteger [251];
+		
+		DP[0]=BigInteger.ZERO;
+		DP[1]=BigInteger.ONE;
+		DP[2]=BigInteger.valueOf(3);
+		for(int i=3;i<=250;i++)
+		{
+			DP[i]=DP[i-1].add(DP[i-2].multiply(BigInteger.valueOf(2)));
+		}
+		String N=br.readLine();
+		do {
+		System.out.println(DP[Integer.parseInt(N)]);
+		}while((N=br.readLine())!=null);
+	}
+}
